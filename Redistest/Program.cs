7edf9c1694620a771f6a03d9640e1221525be2f7
ian simpson <iansimpson55@hotmail.com>
2020-06-17 12:32:24 +0100

    added config and core code

--- Redistest/Program.cs
+++ Redistest/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using StackExchange.Redis;
+using Microsoft.Extensions.Configuration;
 
 namespace Redistest
 {
@@ -6,7 +8,84 @@ namespace Redistest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            InitializeConfiguration();
+
+            // Connection refers to a property that returns a ConnectionMultiplexer
+            // as shown in the previous example.
+            IDatabase cache = lazyConnection.Value.GetDatabase();
+
+            // Perform cache operations using the cache object...
+
+            // Simple PING command
+            string cacheCommand = "PING";
+            Console.WriteLine("\nCache command  : " + cacheCommand);
+            Console.WriteLine("Cache response : " + cache.Execute(cacheCommand).ToString());
+
+            // Simple get and put of integral data types into the cache
+            cacheCommand = "GET Message";
+            Console.WriteLine("\nCache command  : " + cacheCommand + " or StringGet()");
+            Console.WriteLine("Cache response : " + cache.StringGet("Message").ToString());
+
+            cacheCommand = "SET Message \"Hello! The cache is working from a .NET Core console app!\"";
+            Console.WriteLine("\nCache command  : " + cacheCommand + " or StringSet()");
+            Console.WriteLine("Cache response : " + cache.StringSet("Message", "Hello! The cache is working from a .NET Core console app!").ToString());
+
+            // Demonstrate "SET Message" executed as expected...
+            cacheCommand = "GET Message";
+            Console.WriteLine("\nCache command  : " + cacheCommand + " or StringGet()");
+            Console.WriteLine("Cache response : " + cache.StringGet("Message").ToString());
+
+            // Get the client list, useful to see if connection list is growing...
+            cacheCommand = "CLIENT LIST";
+            Console.WriteLine("\nCache command  : " + cacheCommand);
+            Console.WriteLine("Cache response : \n" + cache.Execute("CLIENT", "LIST").ToString().Replace("id=", "id="));
+
+            lazyConnection.Value.Dispose();
+        }
+
+        /// <summary>
+        /// Lazy loading Redis connection
+        /// </summary>
+        /// <typeparam name="ConnectionMultiplexer"></typeparam>
+        /// <returns></returns>
+        private static Lazy<ConnectionMultiplexer> lazyConnection = new Lazy<ConnectionMultiplexer>(() =>
+        {
+            string cacheConnection = Configuration[SecretName];
+            return ConnectionMultiplexer.Connect(cacheConnection);
+        });
+
+        /// <summary>
+        /// Redis Connection
+        /// </summary>
+        /// <value></value>
+        public static ConnectionMultiplexer Connection
+        {
+            get
+            {
+                return lazyConnection.Value;
+            }
+        }
+
+        /// <summary>
+        /// Configuration
+        /// </summary>
+        /// <value></value>
+        private static IConfigurationRoot Configuration { get; set; }
+
+        /// <summary>
+        /// Secret
+        /// </summary>
+        const string SecretName = "CacheConnection";
+
+        /// <summary>
+        /// Function to initalise configuration
+        /// </summary>
+        private static void InitializeConfiguration()
+        {
+            var builder = new ConfigurationBuilder()
+                .AddUserSecrets<Program>();
+
+            Configuration = builder.Build();
         }
     }
 }
