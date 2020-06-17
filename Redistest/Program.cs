4d1d067006ff3781eb6116c6dcd819fcf6a38600
ian simpson <iansimpson55@hotmail.com>
2020-06-17 11:24:21 +0100

    init

---
+++ Redistest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Redistest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
